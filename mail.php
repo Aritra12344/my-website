8d05be25a4e9b6e55a742caeddd32fc61de1292c
Aritra12344 <83257965+Aritra12344@users.noreply.github.com>
2021-09-14 06:28:33 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php //get data from form 
+    $name = $_POST['name']; 
+    $email= $_POST['email']; 
+    $message= $_POST['message']; 
+    $to = "aritraghosh625@gmail.com"; 
+    $subject = "Contact mail"; 
+    $txt ="Name = ". $name . "\r\n Email = " . $email . "\r\n Message =" . $message; 
+    $headers = "From: noreply@yoursite.com" . "\r\n" . "CC: somebodyelse@example.com"; 
+    
+    if($email!=NULL){ mail($to,$subject,$txt,$headers); } 
+//redirect header
+("Location:thankyou.html"); ?>
